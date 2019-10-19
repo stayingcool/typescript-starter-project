4d2c57ef9aa9951a1186482027718493de26f5cb
Nisar Ahmed <stayingcool@gmail.com>
2019-10-20 08:33:42 +1100

    first commit

---
+++ build/index.js
@@ -0,0 +1,5 @@
+const helloPlace = (place) => {
+    return `Hello ${place}!`;
+};
+document.write(helloPlace('World'));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const helloPlace = (place: string): string => {
+    return `Hello ${place}!`;
+  }
+  document.write(helloPlace('World'));
